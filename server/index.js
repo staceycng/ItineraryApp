35de29ccded55254952f2db3139095bd0967303d
Darth Varg <cbvargas123@gmail.com>
2020-04-21 20:58:44 -0700

    Added yelp.js file in router folder for all yelp queries; add yelp file path to server index file;

--- server/index.js
+++ server/index.js
@@ -8,34 +8,37 @@ const port = 3000;
 // server api routes
 const users = require('./router/users.js');
 const itinerary = require('./router/itinerary.js');
+const yelp = require('./router/yelp.js');
+
 
 //mongo database URI string
 const db = require('../config/keys.js').mongo_uri;
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 
 app.use("/users", users);
 app.use("/itinerary", itinerary);
+app.use("/yelp", yelp);
+
 
 
 mongoose
-.connect(db, { 
-	useNewUrlParser: true,
-	useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
 
 
 app.listen(port, (err) => {
     if (err) {
         console.log(err);
-    }
-    else {
+    } else {
         console.log(`Server is listening on port ${port}`);
     }
 })
---
+++ server/router/yelp.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+// router
+//     .route('/')
+//     .get('/test', controllers.function)
+
+module.exports = router
