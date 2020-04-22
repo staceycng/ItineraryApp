c192c6738641e4800cf4ffd40383e9ec13cb74d4
Darth Varg <cbvargas123@gmail.com>
2020-04-21 23:53:21 -0700

    added url to yelp.js

--- server/router/yelp.js
+++ server/router/yelp.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-// router
-//     .route('/')
-//     .get('/test', controllers.function)
+router
+    .route('/events')
+    .get((req, res) => {
+        res.status(200).send("Yelp route is connected!");
+    })
+
+// https://api.yelp.com/v3
 
 module.exports = router
