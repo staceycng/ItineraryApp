7a224fa44b7ba8c99b48e49b3f83a5d693f3170a
Darth Varg <cbvargas123@gmail.com>
2020-04-22 11:41:34 -0700

    test route for yelp api call works; will hash out the rest of the routes now

--- server/routers/yelp.js
+++ server/routers/yelp.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch')
 require('dotenv').config()
+
+const yelp_url = 'https://api.yelp.com/v3'
+const options = {
+    headers: {
+        'Authorization': `Bearer ${process.env.YELP_API_KEY}`
+    }
+}
 
 router
     .route('/events')
-    .get((req, res) => {
-        res.status(200).send("Yelp route is connected!");
-        // console.log(process.env.YELP_API_KEY)
+    .get(async(req, res) => {
+        await fetch(`${yelp_url}/businesses/north-india-restaurant-san-francisco`, options)
+            .then((response) => response.json())
+            .then((data) => res.status(200).send(data))
     })
-
-// https://api.yelp.com/v3
 
 module.exports = router
