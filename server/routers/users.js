363383a53027b33d7ac224c36429fcf3f1592328
Darth Varg <cbvargas123@gmail.com>
2020-04-23 10:56:48 -0700

    Connected User Model to User route

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User.js');
+
+
 
 router.get("/test", (req, res) => {
     res.status(200).send("test users route");
