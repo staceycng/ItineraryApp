6bcf879272a3c6218fd1fb736997f2ace89b9464
Darth Varg <cbvargas123@gmail.com>
2020-04-24 12:40:12 -0700

    fixed mode warning for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,27 @@ const path = require('path');
 
 module.exports = {
     entry: './client/src/index.jsx',
+    mode: 'production',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'client/dist'),
     },
     module: {
         rules: [{
-            test: /\.m?jsx$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
-                    plugins: ['@babel/plugin-proposal-class-properties']
+                test: /\.m?jsx$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
                 }
-            }
-        },
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },]
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
     }
 };
