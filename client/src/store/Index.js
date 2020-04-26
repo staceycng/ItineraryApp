e5f6e3033d8f32616ff8413ddb0d301dd4406f4e
Darth Varg <cbvargas123@gmail.com>
2020-04-26 12:11:15 -0700

    wrapped redux devtools in an if statement

--- client/src/store/Index.js
+++ client/src/store/Index.js
@@ -6,9 +6,16 @@ var initialState = {
 
 };
 
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
-const store = createStore(rootReducer, initialState);
+let store;
+
+if (process.env.NODE_ENV === 'production') {
+    store = createStore(rootReducer, initialState);
+} else {
+    const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    store = createStore(rootReducer, initialState, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+}
+
 
 export default store;
